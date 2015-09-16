acabfe894a9ba5e06b808fe5086f2e74841f50e8
NirwanaChandra <nirwana.atik@gmail.com>
2015-09-16 16:58:18 +0100

    Create Koneksi_data.php

---
+++ Koneksi_data.php
@@ -0,0 +1,14 @@
+<?php
+$host = "mysql.idhostinger.com";
+$localhost ="u167937699_eks";
+$passwordhost = "Karina24";
+$db_nama ="u167937699_eks";
+$tbl_name ="web_eks4";
+
+
+	mysql_connect ("localhost","root","") or die ("koneksi data base gagal");
+	
+	//untuk kineksi dengan data base
+	mysql_select_db("db_web_eks4") or die ("database tidak ada, silahkan cek database anda");
+	
+?>
