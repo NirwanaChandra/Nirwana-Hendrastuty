a69aa50fff1e7d7ad75bed23107d5911efdb84d6
NirwanaChandra <nirwana.atik@gmail.com>
2015-09-16 16:59:17 +0100

    Create menu_kiri.php

---
+++ menu_kiri.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="desain_css.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="5">
+  <tr>
+    <td><div align="center" class="css">
+      <div align="center">Menu Utama</div>
+    </div></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=home">Home</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=entry">Entry Data</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=viwe">View Data</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=sistem">Sistem Pemesanan</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=profil">Profil</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=input">Input data</a></td>
+  </tr>
+  <tr>
+    <td><a href="?menu=transaksi">Data Transaksi</a></td>
+  </tr>
+</table>
+</body>
+</html>
