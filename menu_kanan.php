8a173aefe0ae9d8011d311bf87062cf3515d7f99
NirwanaChandra <nirwana.atik@gmail.com>
2015-09-16 16:58:46 +0100

    Create menu_kanan.php

---
+++ menu_kanan.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="file:///F|/AppServ/www/WEB_Ekstensi4/desain_css.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="5">
+  <tr>
+    <td align="left" valign="top" class="judul"><div align="center">Link</div></td>
+  </tr>
+  <tr>
+    <td><a href="www.google.com" target="_blank">Google</a></td>
+  </tr>
+  <tr>
+    <td><a href="www.facebook.com" target="_blank">Facebook</a></td>
+  </tr>
+  <tr>
+    <td><a href="www.twitter.com" target="_blank">Twitter</a></td>
+  </tr>
+  <tr>
+    <td><a href="teknokrat.ac.id" target="_blank">Teknokrat</a></td>
+  </tr>
+</table>
+</body>
+</html>
