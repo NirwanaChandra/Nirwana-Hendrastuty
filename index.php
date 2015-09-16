b5e08c1cade56407369cd2f200a5056f444a5f8e
NirwanaChandra <nirwana.atik@gmail.com>
2015-09-16 16:56:36 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	include "koneksi_data.php";
+?>
+<table width="100%" border="1" cellspacing="0" cellpadding="5">
+  <tr>
+    <td colspan="3"><img src="Untitled-2 copy.jpg" width="99%" height="300" longdesc="Untitled-2 copy.jpg" /></td>
+  </tr>
+  <tr>
+    <td width="20%"><? include "menu_kiri.php"; ?></td>
+    <td width="60%" align="left" valign="top"><? include "isi.php"; ?></td>
+    <td width="20%"><? include "menu_kanan.php"; ?></td>
+  </tr>
+  <tr>
+    <td colspan="3">Copyright : Tugas Pemerograman Web</td>
+  </tr>
+</table>
+</body>
+</html>
